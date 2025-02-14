d281c1e9fca602c614f7c64f9e53339b5b274ff3
arsen <you@example.com>
2025-02-14 10:14:03 +0300

    is_zero

---
+++ is_lenght_zero.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+if [ -n "arsen" ]; then
+    echo "String lenght greater than zero "
+else
+    echo "String length iz zero"
+fi        
---
+++ is_lenght_zero2.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+if [ -z "arsen" ]; then
+    echo "String length iz zero "
+else
+    echo "String lenght greater than zero "
+fi        
